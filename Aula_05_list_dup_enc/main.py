700449f326b989ef93a07bbe11eb8dd73b18eb73
Alisson <Alissonbpz@gmail.com>
2026-08-27 19:51:58 -0300

    Lista duplamente encadeada

---
+++ Aula_05_list_dup_enc/main.py
@@ -0,0 +1,48 @@
+# =====================================================================
+# 1. DEFINIÇÃO DO NÓ DUPLO (NODE DUPLO)
+# =====================================================================
+class NodeDuplo:
+    def __init__(self, dado):
+        self.dado = dado
+        self.anterior = None
+        self.proximo = None
+
+
+# =====================================================================
+# 2. DEFINIÇÃO DA LISTA DUPLAMENTE ENCADEADA (ESQUELETO)
+# =====================================================================
+class ListaDuplamenteEncadeada:
+    def __init__(self):
+        self.cabeca = None
+        self.cauda = None
+
+    def esta_vazia(self) -> bool:
+        return self.cabeca is None
+
+    def inserir_inicio(self, valor):
+        novo_no = NodeDuplo(valor)
+        if self.esta_vazia():
+            self.cabeca = novo_no
+            self.cauda = novo_no
+        else:
+            novo_no.proximo = self.cabeca
+            self.cabeca.anterior = novo_no
+            self.cabeca = novo_no
+
+    def inserir_fim(self, valor):
+        novo_no = NodeDuplo(valor)
+        if self.esta_vazia():
+            self.cabeca = novo_no
+            self.cauda = novo_no
+
+        else:
+            novo_no.anterior = self.cauda
+            self.cauda.proximo = novo_no
+            self.cauda = novo_no
+    def imprimir_frente(self):
+        print(self.cabeca.dado)
+
+    def imprimir_tras(self):
+        print(self.cauda.dado)
+
+    
